57b16320f5eaf1a28853bfd2832f0b8065fc2f9b
Roberto Barros <roberto.f.barros@gmail.com>
2021-11-25 21:42:12 -0300

    without ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
